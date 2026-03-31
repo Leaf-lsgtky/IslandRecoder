6267a4faf3873be0d3994e048ddef281bd921219
root <root@localhost.localdomain>
2026-03-31 14:23:35 +0000

    增加了更多不可用的功能

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.island.recorder"
         minSdk = 24
         targetSdk = 35
-        versionCode = 6
-        versionName = "1.2.1"
+        versionCode = 7
+        versionName = "1.2.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         
